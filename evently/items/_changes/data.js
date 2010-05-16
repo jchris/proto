3c9cb8ac949c8cbf0de907c3b283f8cc534ecd71
Chris Anderson <jchris@apache.org>
2010-05-15 22:35:37 -0700

    display attached images

--- evently/items/_changes/data.js
+++ evently/items/_changes/data.js
@@ -1,10 +1,21 @@
 function(data) {
-  $.log(data)
+  var db = $$(this).app.db;
+  // $.log(data)
+  function attachments(doc) {
+    var as = [], a = doc._attachments;
+    if (a) {
+      for (var n in a) {
+        as.push({name: n, path: ["",db.name,doc._id,n].join('/')});
+      }
+      return as;
+    }
+  };
   var p;
   return {
     items : data.rows.map(function(r) {
       p = r.value.profile;
       p.message = r.value.message;
+      p.atts = attachments(r.value);
       return p;
     })
   }
