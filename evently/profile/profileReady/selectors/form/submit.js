3d25a997157507b22398abba4be1c1b7ef2b691a
Chris Anderson <jchris@apache.org>
2010-05-15 23:16:58 -0700

    upload image with message

--- evently/profile/profileReady/selectors/form/submit.js
+++ evently/profile/profileReady/selectors/form/submit.js
@@ -10,15 +10,20 @@ function(e) {
 
   db.saveDoc(doc, {
     success : function() {
-      $("[name=message]", form).val("Uploading file...");
       $("input[name='_rev']", form).val(doc._rev);
-      // thank you cmlenz for Futon's original upload code
-      form.ajaxSubmit({
-        url: db.uri + $.couch.encodeDocId(doc._id),
-        success: function(resp) {
-          $("[name=message]", form).val("");
-        }
-      });
+      var as = $("input[name='_attachments']", form).val();
+      if (as) {
+        $("[name=message]", form).val("Uploading file...");
+        // thank you cmlenz for Futon's original upload code
+        form.ajaxSubmit({
+          url: db.uri + $.couch.encodeDocId(doc._id),
+          success: function(resp) {
+            $("[name=message]", form).val("");
+          }
+        });
+      } else {
+        $("[name=message]", form).val("");
+      }
     }
   });
   return false;
